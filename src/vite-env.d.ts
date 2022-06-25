96462a3d6b6dfd5caa620790eacf9254dd221293
Lucas-Bispo <lukaobispo@gmail.com>
2022-06-25 14:58:47 -0300

    :arrow_up: Dependências de atualização.

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,1 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-export const client = new ApolloClient({
-  uri: import.meta.env.VITE_API_URL,
-  headers: {
-    'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
-  },
-  cache: new InMemoryCache()
-})
+/// <reference types="vite/client" />
