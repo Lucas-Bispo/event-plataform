21d23b525c501ea87437434bd7f75cc66ec2b4fb
Lucas-Bispo <lukaobispo@gmail.com>
2022-06-25 10:21:24 -0300

    :boom: Introduzir mudanças de ruptura.

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,9 @@
-/// <reference types="vite/client" />
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+export const client = new ApolloClient({
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+  },
+  cache: new InMemoryCache()
+})
